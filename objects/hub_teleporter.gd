16b40480b3dc092c33da83011cca3a20019abc03
catbuddy1028 <catbuddy.personal@gmail.com>
2025-05-28 11:03:16 +0200

    finished lvl 2

--- objects/hub_teleporter.gd
+++ objects/hub_teleporter.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		get_tree().change_scene_to_file("res://objects/hub.tscn")
 		Global.clear_quiz()
-		Global.game_stage = 1
+		Global.game_stage += 1
